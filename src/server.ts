c7a01987f6152421056badca2dd79bad6dd9a7cd
nguyenthanhluu181097 <nguyenthanhluu181097@gmail.com>
2024-10-30 09:55:21 +0700

    change cors policy

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    // origin: "http://localhost:4200",
+    origin: true,
   })
 );
 app.use("/api/products", productsRouter);
