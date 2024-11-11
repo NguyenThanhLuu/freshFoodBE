476e15ad2e16f4c70520a46bcbf619bea0b1707d
nguyenthanhluu181097 <nguyenthanhluu181097@gmail.com>
2024-11-11 11:01:19 +0700

    prevent change admin information

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -6,6 +6,7 @@ import {
   updateUser,
 } from "../controllers/users.controller";
 import authMid from "../middlewares/auth.mid";
+import adminValidateAuth from "../middlewares/admin-validate.auth";
 import multer from "multer";
 
 const router = Router();
@@ -15,6 +16,9 @@ const upload = multer({ storage });
 router.post("/login", handleLogin);
 router.post("/signUp", handleSignUp);
 router.post("/newToken", handleCreateNewToken);
-router.use(authMid).patch("/update", upload.single("avatarFile"), updateUser);
+
+router.use(authMid);
+router.use(adminValidateAuth);
+router.patch("/update", upload.single("avatarFile"), updateUser);
 
 export default router;
